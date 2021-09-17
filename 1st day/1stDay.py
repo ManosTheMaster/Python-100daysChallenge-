979030f08f227952250468befac6dbabd7bb8f6e
ManosTheMaster <themagicsport@gmail.com>
2021-09-17 17:24:47 +0100

    1st commit and 1st day of 100 days challenge

---
+++ 1st day/1stDay.py
@@ -0,0 +1,29 @@
+#Print function
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#equal result
+print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
+
+print("Hello" + " " + "Manos")
+
+#make quotes visible in printing
+print(" \" Hello world \" ")
+
+#Input function
+input("What is your name")
+
+#Combine print and input
+print("Hello " + input("What is your name?"))
+
+#Program that reads a name and prints the length of it
+print(len(input("What is your name?")))
+
+#Variables
+name = input("What is your name?")
+print("Hello "+ name)
+
+name = input("What is your name?")
+length = len(name)
+print(length)
