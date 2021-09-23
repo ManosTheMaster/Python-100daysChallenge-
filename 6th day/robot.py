c0e82cfcb725a867c7ec1fda4085027a0e5a3375
ManosTheMaster <themagicsport@gmail.com>
2021-09-23 18:05:20 +0100

    add source

--- 6th day/robot.py
+++ 6th day/robot.py
@@ -1,3 +1,4 @@
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
 def pass_a_hurlder():
     turn_left()
     move()
